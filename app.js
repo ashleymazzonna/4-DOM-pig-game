0993eae441430b9de3452c5c11667ead4a6adfdf
ashleymazzonna <ashleym5243@gmail.com>
2019-05-09 00:01:20 -0400

    update to player panels - toggle active class; remove display for next player

--- app.js
+++ app.js
@@ -38,6 +38,15 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     } else {
         // next player 
         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        document.querySelector.querySelector('.dice').style.display = 'none';
     }
 })
 
